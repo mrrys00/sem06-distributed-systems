59420282339cf568026bebe6ac0a38491afb3e8a
Szymon Ryś <rysszymon00@gmail.com>
2023-06-21 15:18:47 +0200

    *add final version

--- lab07/main5.py
+++ lab07/main5.py
@@ -1,36 +1,55 @@
 import subprocess
-import threading
+import os
+from threading import Thread
 from time import sleep
 from kazoo.client import KazooClient
 from kazoo.recipe.watchers import ChildrenWatch
 
 GRAPHICAL_APP_PATH = "/usr/bin/gedit"
-process = None
-t1 = None
 
-def my_job():
-    print("serio startuje się apka")
-    process = subprocess.Popen([GRAPHICAL_APP_PATH, "&"], shell=True)
+class CustomThread(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.value = None
+
+    def run(self):
+        sleep(1)
+        self.value = subprocess.Popen([GRAPHICAL_APP_PATH, "&"], shell=True)
+
+threads_ = []
 
 def start_external_app():
     try:
-        t1=threading.Thread(target=my_job)
-        t1.start()
-        sleep(1)
-        print(f"created process: {process.pid}")
+        t = CustomThread()
+        t.start()
+        t.join()
+        threads_.append(t.value)
     except:
-        print("no nie odpala się")
+        pass
 
 def stop_external_app():
     try:
-        process.terminate()
+        print(threads_)
+        t = threads_[len(threads_)-1]
+        print(f"process to be terminated {t}")
+        t.terminate()
     except:
         print("no nie zamyka się")
 
 
-def display_num_children(children):
-    num_children = len(children)
-    print("Aktualna ilość potomków: {}".format(num_children))
+def display_num_children(zk):
+    last_clihd_num = 0
+    while True:
+        num_children = -1
+        try:
+            num_children = len(zk.get_children("/z/"))
+        except:
+            pass
+        if num_children > last_clihd_num:
+            print("Aktualna ilość potomków: {}".format(num_children))
+            os.system(f"zenity --info --text={num_children}")
+        
+        last_clihd_num = num_children
 
 
 def display_tree_structure(zk, path="/"):
@@ -60,7 +79,9 @@ def main():
 
     display_tree_structure(zk)
 
-    ChildrenWatch(client=zk, path="/z", func=watch_children)
+    # ChildrenWatch(client=zk, path="/z", func=watch_children)
+    t1=Thread(target=display_num_children, args=(zk,))
+    t1.start()
     try:
         while True:
             zk.exists("/z", watch=watch_znode)
